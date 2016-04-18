650c35882ce3f67d64d848bb6197f363974434b9
Michael D Worley <mworley17@lab-sci-304-20.local>
2016-04-17 19:28:19 -0700

    Initial Commit

---
+++ RoadTripHistory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadTripHistory
+//
+//  Created by Michael D Worley on 4/17/16.
+//  Copyright © 2016 Michael D Worley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
